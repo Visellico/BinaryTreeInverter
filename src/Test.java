94179d92e32ac408a5cdbad9192b30f028026421
Henry Farr <hfarr80@yahoo.com>
2017-12-04 23:17:48 -0500

    Updated to run more tests, and cleaned up the test.

--- src/Test.java
+++ src/Test.java
@@ -11,13 +11,26 @@ public class Test {
 
     public static void main(String[] args) {
 
-        List<Integer> ints = Arrays.asList(4, 2, 7, 1, 3, 6, 9);
-        Tree balancedTree = new Tree(ints);
+        new Test().go();
+    }
 
-        System.out.println(balancedTree);
-        balancedTree.reverse();
-        System.out.println(balancedTree);
+    public void go() {
+        runTest(Arrays.asList(4,2,7,1,3,6,9));
+        System.out.println();
+        runTest(Arrays.asList(1,2,3,4,5,6));
+        System.out.println();
+        runTest(Arrays.asList(6,5,3,2,4,8));
+    }
 
+    //TODO compare reversed output of the tree to an expected reversed output
+    public void runTest(List<Integer> list) {
+        Tree tree;
+
+        tree = new Tree(list);
+
+        System.out.println(tree);
+        tree.reverse();
+        System.out.println(tree);
     }
 
 }
